2faec76868940d7c3a49b77c505db006de2c8204
nawright16 <nawright16@gmail.com>
2023-07-11 16:07:11 -0400

    added forgot password link, changed a little styling

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -5,10 +5,11 @@ const LoginForm = (props) => {
     return <div className='login'>
         <form className='loginform'>
             <div className='top'>
-                <h1>Login</h1>
+                <h1>Please Sign In</h1>
                 <p className='username'><input type="text" placeholder='Enter username' /></p>
                 <p className='password'><input type="password" placeholder='Enter password' /></p>
-                <input className="submit" type="submit" value="Log In"/>
+                <input className="submit" type="submit" value="Login" />
+                <p><a href="" className='forgot'>Forgot Password</a></p>
             </div>
             <div className='bottom'>
                 <input type="button" value="Sign up for a FREE account!" />
