b9c731c7ac9ac8103e2a6505d715dd5677f788d4
nawright16 <nawright16@gmail.com>
2023-07-12 12:40:53 -0400

    added useState and handle submit functions

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import  { useState, React }  from 'react';
 import { Link } from 'react-router-dom';
 import './LoginForm.css'
 
 const LoginForm = () => {
+    const [userName, setUserName] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        postMessage('', {userName, password})
+    }
+
     return <div className='login'>
-        <form className='loginform'>
+        <form className='loginform' onSubmit={handleSubmit}>
             <div className='top'>
                 <h1>Please Sign In</h1>
-                <p className='username'><input type="text" placeholder='Enter username' /></p>
-                <p className='password'><input type="password" placeholder='Enter password' /></p>
+                <p className='username'><input type="text" placeholder='Enter username' onChange={(e) => setUserName(e.target.value)} /></p>
+                <p className='password'><input type="password" placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} /></p>
                 <input className="submit" type="submit" value="Login" />
                 <p><a href="" className='forgot'>Forgot Password</a></p>
             </div>
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import { useState, React } from 'react'
 import './SignUp.css'
 
 const SignUp = () => {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        postMessage('', {name, email, password})
+    }
+
     return (
         <div>
              <div className='signup'>
-                <form className='signupform'>
+                <form className='signupform' onSubmit={handleSubmit}>
                     <div className='top'>
                         <h1>Sign Up</h1>
-                        <p className='firstname'><input type="text" placeholder='Enter your first name' /></p>
-                        <p className='lastname'><input type="text" placeholder='Enter your last name' /></p>
-                        <p className='email'><input type="text" placeholder='Enter your email' /></p>
-                        <p className='password'><input type="password" placeholder='Enter password' /></p>
+                        <p className='name'><input type="text" placeholder='Enter your name' onChange={(e) => setName(e.target.value)} /></p>
+                        <p className='email'><input type="text" placeholder='Enter your email' onChange={(e) => setEmail(e.target.value)} /></p>
+                        <p className='password'><input type="password" placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} /></p>
                         <p className='password'><input type="text" placeholder='Confirm password' /></p>
                         <input className="submit" type="submit" value="Enter" />
 
