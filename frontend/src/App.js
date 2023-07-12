b4108d797733dc2e7f474a50986af70c0408e65a
nawright16 <nawright16@gmail.com>
2023-07-12 09:30:07 -0400

    Added BrowserRouter, routes, route to app.js file and paths for login and signup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import SignUp from './components/SignUp/SignUp';
 import LoginPage from './components/LoginPage/LoginPage';
 import LoginForm from './components/LoginForm/LoginForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
-        <LoginForm />
+       <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />}></Route>
+          <Route path='/signup' element={<SignUp />}></Route>
+        </Routes>
+       </BrowserRouter>
+        
        
     </div>
   );
