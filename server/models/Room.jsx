f13bf15a793e8ecf5263c3b0523524f724081cfa
nawright16 <nawright16@gmail.com>
2023-07-16 15:55:33 -0400

    deleted server folder, starting from scratch

--- server/models/Room.jsx
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-
-})
-
-module.exports = mongoose.model("Room", roomSchema);
