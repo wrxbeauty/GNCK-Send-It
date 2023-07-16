1626b29802b5e3e7dddf8e022a27dbcda019d879
nawright16 <nawright16@gmail.com>
2023-07-16 12:51:39 -0400

    adding password hash js-sha256

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const sha256 = require('js-sha256')
 
 exports.signup = async (req,res) => {
     const {name, email, password} = req.body;
     
     if (password.length < 8 ) throw "Password needs to be at least 8 characters"
 
+    const user = new User({name, email, password: sha256(password + process.env.SALT)});
+
+    await user.save()
 };
 
 exports.login = async (req,res) => {};
