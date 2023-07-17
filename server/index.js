ef6670772d0e86b0a3da1c83833a7b8761fdd33d
nawright16 <nawright16@gmail.com>
2023-07-17 14:07:44 -0400

    setting up socket.io on index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv')
 const app = express();
 require("dotenv").config();
 const userRoute = require('./routes/userRoute');
+const socket = require('socket.io')
 
 const PORT = process.env.PORT || 5001
 
@@ -13,15 +14,15 @@ app.use(express.json());
 
 app.use("/api/auth", userRoute)
 
-mongoose.connect(process.env.DB_CONNECTION_URI,{
+mongoose.connect(process.env.DB_CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     console.log("Successfully connected to MongoDB");
 })
-.catch((err) => {
-    console.log(err.message)
-});
+    .catch((err) => {
+        console.log(err.message)
+    });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -29,3 +30,32 @@ app.use("/api/messages", messageRoutes);
 const server = app.listen(process.env.PORT, () => {
     console.log("Server is running like Usain Bolt!")
 })
+
+// The following code setups the sockets for the messages to go back and forth in real time.
+
+
+// This code sets up the socket server, then uses CORS to set the host and uses the credentials for a handshake.
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+// Map is used to help store users and make them accessible during app usage.
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    });
+});
